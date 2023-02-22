1c9f572f7891c35999ee1908cd7f841576ccf9a8
BurakOrtakuz <83423289+BurakOrtakuz@users.noreply.github.com>
2023-02-22 17:12:40 +0300

    Hesap Makinesi

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        dortİslem dortİslem = new dortİslem();
+        int toplam = dortİslem.Bol(10, 1);
+        System.out.println("Sonuç:" + toplam);
+
+
+    }
+}
